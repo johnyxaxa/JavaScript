e9e0aa6bb6c48e3b5b3c6020c32d04135360d280
Johny Xaxa <johnyxaxa@pm.me>
2024-04-22 03:41:00 +0530

    Capitalize a string

--- Functions/funcBasics.js
+++ Functions/funcBasics.js
@@ -13,3 +13,14 @@ function multiply(x,y) {
 }   
 
 multiply(3,7);
+
+// Function called capitalize that takes a string and returns that string with only first
+// letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTH.
+
+function capitalize(anyString) {
+    return anyString.charAt(0).toUpperCase() + anyString.slice(1);
+}
+
+let string = "capitalize 1st c";
+let capitalizedString = capitalize(string);
+console.log(capitalizedString);
